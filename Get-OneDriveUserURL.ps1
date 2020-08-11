5816c56a11a3bdf8baf9df1013e26d81487a1f96
Deivid de Foggi <deividfoggi@hotmail.com>
2020-08-11 17:45:24 -0300

    Feature to find groupsite URL by group displayname

--- Get-OneDriveUserURL.ps1
+++ Get-OneDriveUserURL.ps1
@@ -62,13 +62,29 @@ Function Send-GraphRequest{
 Function Get-OneDriveUserURL{
 
     param(
-        $UserPrincipalName
+        $UserPrincipalName,
+        $GroupSiteDisplayName
     )
 
     $accessToken = Get-OauthToken -clientID $clientID -tenantID $tenantID -clientSecret $clientSecret
-    $graphRequest = Send-GraphRequest -Method Get -BearerToken $accessToken.access_token -Path "/users/$($userPrincipalName)/drive"
+    $oneDriveURLRequest = Send-GraphRequest -Method Get -BearerToken $accessToken.access_token -Path "/users/$($userPrincipalName)/drive"
+    $groupRequestFilter = '?$filter=displayName eq '
+    $groupRequestFilter += "'$($GroupSiteDisplayName)'"
+    $groupRequest = Send-GraphRequest -Method Get -BearerToken $accessToken.access_token -Path "/groups$($groupRequestFilter)"
+    $groupSiteRequest = Send-GraphRequest -Method Get -BearerToken $accessToken.access_token -Path "/groups/$($groupRequest.value.Id)/sites/root"
 
-    return $graphRequest
+    $lastURLDir = $oneDriveURLRequest.webUrl.split("/").split("/")[-1]
+    $oneDriveURL = $oneDriveURLRequest.webUrl.Replace($lastURLDir,"")
+
+    $obj = New-Object psobject 
+    $obj = @{
+        OneDriveOnwer = $oneDriveURLRequest.owner.user.displayName
+        OneDriveURL = $oneDriveURL
+        GroupSiteDisplayName = $groupSiteRequest.displayName
+        GroupSiteURL = $groupSiteRequest.webUrl
+    }
+
+    return $obj
 }
 
 Add-Type -AssemblyName System.Windows.Forms
@@ -76,26 +92,27 @@ Add-Type -AssemblyName System.Drawing
 
 $form = New-Object System.Windows.Forms.Form
 $form.Text = 'OneDrive URL finder'
-$form.Size = New-Object System.Drawing.Size(800,220)
-$form.MinimumSize = New-Object System.Drawing.Size(800,220)
-$form.MaximumSize = New-Object System.Drawing.Size(800,220)
+$form.Size = New-Object System.Drawing.Size(800,440)
+$form.MinimumSize = New-Object System.Drawing.Size(800,440)
+$form.MaximumSize = New-Object System.Drawing.Size(800,440)
 $form.StartPosition = 'CenterScreen'
 $form.AcceptButton = $okButton
 
 $okButton = New-Object System.Windows.Forms.Button
-$okButton.Location = New-Object System.Drawing.Point(75,140)
+$okButton.Location = New-Object System.Drawing.Point(280,73)
 $okButton.Size = New-Object System.Drawing.Size(75,23)
 $okButton.Text = 'OK'
 $okButton_OnClick = {    
     $form.Text = 'Contacting Microsoft Graph...'
-    $oneDriveURLRequest = Get-OneDriveUserURL -UserPrincipalName $textBoxUPN.Text
-    if(!$oneDriveURLRequest.webUrl){
-        $labelResultOwner.Text = $oneDriveURLRequest
+    $graphRequest = Get-OneDriveUserURL -UserPrincipalName $textBoxUPN.Text -GroupSiteDisplayName $textBoxGroupSiteDisplayName.Text
+    if(!$graphRequest.OneDriveURL){
+        $labelResultOwner.Text = $graphRequest.OneDriveOnwer
     }
     else{
-        $labelResultOwner.Text = $oneDriveURLRequest.owner.user.displayName
-        $lastDir = $oneDriveURLRequest.webUrl.split("/").split("/")[-1]
-        $labelResultOneDriveURL.Text = $oneDriveURLRequest.webUrl.Replace($lastDir,"")
+        $labelResultOwner.Text = $graphRequest.OneDriveOnwer
+        $labelResultOneDriveURL.Text = $graphRequest.OneDriveURL
+        $labelGroupSiteDisplayName.Text = $graphRequest.GroupSiteDisplayName
+        $labelGroupSiteURL.Text = $graphRequest.GroupSiteURL
     }
     $form.Text = 'OneDrive URL finder'
     $form.Update()
@@ -104,7 +121,7 @@ $okButton.add_Click($okButton_OnClick)
 $form.Controls.Add($okButton)
 
 $cancelButton = New-Object System.Windows.Forms.Button
-$cancelButton.Location = New-Object System.Drawing.Point(150,140)
+$cancelButton.Location = New-Object System.Drawing.Point(360,73)
 $cancelButton.Size = New-Object System.Drawing.Size(75,23)
 $cancelButton.Text = 'Cancel'
 $cancelButton.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
@@ -122,29 +139,69 @@ $textBoxUPN.Location = New-Object System.Drawing.Point(10,30)
 $textBoxUPN.Size = New-Object System.Drawing.Size(260,20)
 $form.Controls.Add($textBoxUPN)
 
+$labelGroupSiteDisplayName = New-Object System.Windows.Forms.Label
+$labelGroupSiteDisplayName.Location = New-Object System.Drawing.Point(10,55)
+$labelGroupSiteDisplayName.Size = New-Object System.Drawing.Size(280,20)
+$labelGroupSiteDisplayName.Text = "Insert group/Teams site name:"
+$form.Controls.Add($labelGroupSiteDisplayName)
+
+$textBoxGroupSiteDisplayName = New-Object System.Windows.Forms.TextBox
+$textBoxGroupSiteDisplayName.Location = New-Object System.Drawing.Point(10,75)
+$textBoxGroupSiteDisplayName.Size = New-Object System.Drawing.Size(260,20)
+$form.Controls.Add($textBoxGroupSiteDisplayName)
+
+
 $labelOnwerlabel = New-Object System.Windows.Forms.Label
-$labelOnwerlabel.Location = New-Object System.Drawing.Point(10,70)
+$labelOnwerlabel.Location = New-Object System.Drawing.Point(10,100)
 $labelOnwerlabel.Size = New-Object System.Drawing.Size(85,20)
 $labelOnwerlabel.Text = 'Owner:'
 $form.Controls.Add($labelOnwerlabel)
 
 $labelResultOwner = New-Object System.Windows.Forms.TextBox
-$labelResultOwner.Location = New-Object System.Drawing.Point(100,70)
+$labelResultOwner.Location = New-Object System.Drawing.Point(10,120)
 $labelResultOwner.Size = New-Object System.Drawing.Size(675,20)
 $labelResultOwner.ReadOnly = $true
 $form.Controls.Add($labelResultOwner)
 
 $labelOneDriveURLlabel = New-Object System.Windows.Forms.Label
-$labelOneDriveURLlabel.Location = New-Object System.Drawing.Point(10,100)
+$labelOneDriveURLlabel.Location = New-Object System.Drawing.Point(10,150)
 $labelOneDriveURLlabel.Size = New-Object System.Drawing.Size(85,20)
 $labelOneDriveURLlabel.Text = 'OneDrive URL:'
 $form.Controls.Add($labelOneDriveURLlabel)
 
 $labelResultOneDriveURL = New-Object System.Windows.Forms.TextBox
-$labelResultOneDriveURL.Location = New-Object System.Drawing.Point(100,100)
+$labelResultOneDriveURL.Location = New-Object System.Drawing.Point(10,170)
 $labelResultOneDriveURL.Size = New-Object System.Drawing.Size(675,20)
 $labelResultOneDriveURL.ReadOnly = $true
 $form.Controls.Add($labelResultOneDriveURL)
+
+#
+
+$labelGroupSiteDisplayNamelabel = New-Object System.Windows.Forms.Label
+$labelGroupSiteDisplayNamelabel.Location = New-Object System.Drawing.Point(10,200)
+$labelGroupSiteDisplayNamelabel.Size = New-Object System.Drawing.Size(200,20)
+$labelGroupSiteDisplayNamelabel.Text = 'Group Site Display Name:'
+$form.Controls.Add($labelGroupSiteDisplayNamelabel)
+
+$labelGroupSiteDisplayName = New-Object System.Windows.Forms.TextBox
+$labelGroupSiteDisplayName.Location = New-Object System.Drawing.Point(10,220)
+$labelGroupSiteDisplayName.Size = New-Object System.Drawing.Size(675,20)
+$labelGroupSiteDisplayName.ReadOnly = $true
+$form.Controls.Add($labelGroupSiteDisplayName)
+
+$labelGroupSiteURLlabel = New-Object System.Windows.Forms.Label
+$labelGroupSiteURLlabel.Location = New-Object System.Drawing.Point(10,250)
+$labelGroupSiteURLlabel.Size = New-Object System.Drawing.Size(200,20)
+$labelGroupSiteURLlabel.Text = 'Group Site URL:'
+$form.Controls.Add($labelGroupSiteURLlabel)
+
+$labelGroupSiteURL = New-Object System.Windows.Forms.TextBox
+$labelGroupSiteURL.Location = New-Object System.Drawing.Point(10,270)
+$labelGroupSiteURL.Size = New-Object System.Drawing.Size(675,20)
+$labelGroupSiteURL.ReadOnly = $true
+$form.Controls.Add($labelGroupSiteURL)
+
+#
 
 $form.Topmost = $true
 
